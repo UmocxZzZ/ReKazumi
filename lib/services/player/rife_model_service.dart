e9d54f324ca75597ff992eb8b2e2c44fc773af94
UmocxZzZ <995079766@qq.com>
2026-08-11 14:06:25 +0800

    fix bundled RIFE model error import

--- lib/services/player/rife_model_service.dart
+++ lib/services/player/rife_model_service.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'dart:io';
 
 import 'package:crypto/crypto.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
